346f73400b0aa567c48500accab9cd2c80a9e099
Nikita Solovev <solovev.nikita@mail.ru>
2023-07-11 20:16:22 +0500

    feat: исправления работы

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import { motion, AnimatePresence} from 'framer-motion';
 import { useSnapshot } from 'valtio';
+import { CustomButton } from '../components';
 
 import state from '../store'
-
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -17,13 +17,37 @@ const Home = () => {
     <AnimatePresence>
       {snap.intro && (
         <motion.section className="home" {...slideAnimation('left')}>
-          <motion.header>
+          <motion.div {...slideAnimation("down")}>
             <img
-              src="./threejs.png"
+              src="src/public/threejs.png"
               alt='logo'
               className="w-8 h-8 object-contain"
             />
-          </motion.header>
+          </motion.div>
+
+          <motion.div className='home-content' {...headContainerAnimation}>
+            <motion.div>
+              <h1 className='head-text'>
+                LET'S <br className='xl:block hidden' /> DO IT.
+              </h1>
+            </motion.div>
+
+            <motion.div
+              {...headContainerAnimation}
+              className='flex flex-col gap-5'
+            >
+              <p className='max-w-md font-normal text-grey-600 text-base'>
+                Create your uniq and awesome design <strong>GO GO GO</strong>
+              </p>
+
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => state.intro = false}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
+          </motion.div>
         </motion.section>
       )}
     </AnimatePresence>
